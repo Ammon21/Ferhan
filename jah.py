eaf48efa2b0ef2ca4f945c2b5e99a1e679051c99
Ammon21 <ammonb6@gmail.com>
2023-02-07 18:48:53 +0300

    Club Entrance System

--- jah.py
+++ jah.py
@@ -1,2 +1,15 @@
-for i in range(100):
+for i in range(10):
     print("Hello World")
+
+name = input("What is your name: ")
+age = eval(input("what is your age: "))
+
+
+print("Welcome " + name)
+
+if age > 18:
+    print("Get in")
+else:
+    print('go home to your mama')
+
+    
